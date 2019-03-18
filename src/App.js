4238ca319a9e81406f9e576cf713dbd46dd3a3b1
mocove <mocove@gmail.com>
2019-03-18 13:28:04 +0100

    Implemented start/stop and background color.

--- src/App.js
+++ src/App.js
@@ -5,30 +5,61 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { counter: 600 }
+    this.state = { 
+      counter: 600,
+      isRunning: false }
     this.myInterval = null;
-    this.countDown = this.countDown.bind(this);
+    this.tick = this.tick.bind(this);
+    this.startCountDown = this.startCountDown.bind(this);
+    this.stopCountDown = this.stopCountDown.bind(this);
+    this.toggleIsRunning = this.toggleIsRunning.bind(this);
   }
 
   
 
-  countDown() {
-    console.log(this.state.counter);
+  tick() {
     this.setState({
       counter: this.state.counter - 1
     });
   }
 
+  startCountDown() {
+    if (!this.state.isRunning) {
+      this.myInterval = setInterval(this.tick, 1000);
+      this.setState({ isRunning: true });
+    }
+  }
+
+  stopCountDown() {
+    clearInterval(this.myInterval);
+    this.setState({ isRunning: false });
+  }
+
+  toggleIsRunning() {
+    const newState = this.state.isRunning ? 'false' : 'true';
+    this.setState({ isRunning : newState });
+    console.log(this.state.isRunning);
+  }
+
   componentDidMount() {
-    this.myInterval = setInterval(this.countDown, 1000);
+    
   }
   
   
 
   render() {
+    let bgClass = 'stopped';
+    if (this.state.isRunning) {
+      bgClass = 'running';
+    }
+    let className = 'App ' + bgClass
     return (
-      <div className="App">
+      <div className={className}>
         <h1>{ this.state.counter }</h1>
+        <button onClick={ this.startCountDown }>Start CountDown</button>
+        <button onClick={ this.stopCountDown }>Stop CountDown</button>
+        <br />
+        <p>Running: { this.state.isRunning ? 'True' : 'False' }</p>
       </div>
     );
   }
